d802976c30cefe822c6bc1c78738804e10c0d6b7
Tgaray <therongaray@gmail.com>
2022-03-27 22:42:48 +0200

    Section 7
    -Dom manipulation
    -Events
    -Start of game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,20 @@
 'use strict';
 
+/* 
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 12;
+console.log((document.querySelector('.guess').value = 23)); 
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Please input a number!';
+  }
+});
