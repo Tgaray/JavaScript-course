a59af1a73fc5193cd505d35851e633653a201b42
Tgaray <therongaray@gmail.com>
2022-04-04 18:04:02 +0200

    Section 7 Modal
    Select the HTML elements to work with

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,8 +3,12 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
+//Select all the buttons that have this class
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].textContent);
+  //Show the buttons as HTML elements
+  console.log(btnsOpenModal[i]);
+  //Show the text content of each button
+  console.log(btnsOpenModal[i].textContent);
 }
