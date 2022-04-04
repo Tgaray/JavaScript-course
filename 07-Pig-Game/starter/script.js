a58cfd67e50cfce3268c311dd25e1208409168ca
Tgaray <therongaray@gmail.com>
2022-04-04 21:34:35 +0200

    Section 7 Pig Game
    -Selecting of elements
    -Btnroll function for random dice number
    -Show the right dice image
    -Check if its 1 switch active player and add score to the active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,51 @@
 'use strict';
+
+//Selecting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const player = document.querySelectorAll('.player--active');
+let currentScore0 = 0;
+let currentScore1 = 0;
+let bool = true;
+
+console.log(player);
+//Starting conditions
+diceEl.classList.add('hidden');
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+//functions
+
+//Event handlers
+//btnNew.addEventListener('click');
+btnRoll.addEventListener('click', function () {
+  //1. Generate random dice roll
+  let diceRoll = Math.trunc(Math.random() * 6 + 1);
+  console.log(diceRoll);
+
+  //2. Show the corresponding dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${diceRoll}.png`;
+
+  //3. Check for rolled 1: if true switch to next player
+  if (diceRoll !== 1) {
+    //Add dice to the current score
+    if (bool) {
+      current0El.textContent = currentScore0 += diceRoll;
+    } else {
+      current1El.textContent = currentScore1 += diceRoll;
+    }
+  } else {
+    bool = !bool;
+    console.log(bool);
+  }
+  //Check which player is active
+});
+
+//btnHold.addEventListener('click');
