7c502acd1b50098d4ac111dd7802e363e785c971
Tgaray <therongaray@gmail.com>
2022-04-12 23:08:14 +0200

    Section 9 Lesson 103
    Destructuring Arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,63 @@ const restaurant = {
     },
   },
 };
+
+//Lesson 103 Destructuring Arrays
+/* 
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+console.log(a, b, c);
+
+//destructuring an array
+const [x, y, z] = arr;
+console.log(x, y, z);
+//the originla array is not affected we are just destructuring
+console.log(arr);
+ */
+
+/* 
+//destructuring array from restaurant data, and skip one element
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+//sometimes you want to change the data like this:
+//store the main in the temp var
+const temp = main;
+//then overwrite main with the secondary value
+main = secondary;
+//and then store the main in the secondary value to swap the values
+secondary = temp;
+//the values are swapped
+console.log(main, secondary);
+ */
+/* 
+//Switching variables
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+//array destructuring way of swapping values like above
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+//call the order function to order something from the startermenu and mainmenu
+console.log(restaurant.order(2, 0));
+//destructuring the starter and main from the menu
+//This is how we retreive two return values from a function
+//Creating two variables out of one function call
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+//How to destructure the case of a nested array
+const nested = [2, 4, [5, 6]];
+//Grab the number 2 and the 5&6 array within the array
+const [i, , j] = nested;
+console.log(i, j);
+//destructing the nested arrays as to get only the values except the 4 value
+const [value1, , [value3, value4]] = nested;
+console.log(value1, value3, value4);
+
+//Destructuring with default values in the case of a shorter array
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+ */
