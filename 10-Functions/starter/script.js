b56676b263c6e53cf9190943b09230347d4f1061
Tgaray <therongaray@gmail.com>
2022-05-10 22:44:17 +0200

    Section 10 Functions
    -Lesson 129 How passing arguments works (Value vs Reference)
    -Lesson 130 First Class and Higher order functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,15 +1,54 @@
 'use strict';
 
+//Lesson 131 Functions accepting callback functions
+
+/* 
 //Lesson 129 how passing arguments into functions work (primitives and objects in function context)
 
 //Primitive
 const flight = 'FH123';
 
 //Object
-const Theron = {
+const theron = {
   firstName: 'Theron',
   lastName: 'Garay',
+  passport: 584848939,
 };
+
+const checkIn = function (flightNum, passenger) {
+  //flightNum is a completely new/different variable here inside the function, passing the string into the function makes it a copy not the original value
+  flightNum = 'LH999';
+  //The passenger object is overwritten, because what was passed into the function is the reference to the object (passenger is Theron (the same object in the memory heap))
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 584848939) {
+    alert('Checked In');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, theron);
+console.log(flight);
+console.log(theron);
+
+//Passing a primitive or an object into a function is exactly the same as doing the following outside of the function (the primitive is a copy, the object is the same with by reference)
+const flightNum = flight;
+const passenger = theron;
+console.log(flight, theron);
+
+//Good to be aware of this difference, shown by the following example when working with large(r) teams
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+  console.log(person.passport);
+};
+
+//Now we changed the passport number before we check in lets see what happens
+newPassport(theron);
+//The checkIn function is called again but now the passport number is wrong that is because we have two functions manipulation one object
+checkIn(flight, theron);
+//be careful to manipulate one object with different object calls
+*/
 
 /*
 //Lesson 128 default parameters
