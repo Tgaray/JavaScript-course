681e4aedb0d2e3003c4dd626ccae2e67ad2c2273
Tgaray <therongaray@gmail.com>
2022-07-19 19:52:26 +0200

    Section 12 - Numbers, Dates, Intl and Timers
    Lesson 171:
    Rounding numbers
    
    Ints with Math.round/ceil/floor/trunc
    
    And floats with .toFixed(0)

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -292,3 +292,56 @@ console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
 */
+
+//Sqaure root
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+//Cubic root
+console.log(8 ** (1 / 3));
+//Max
+console.log(Math.max(4, 2, 122, 12, 22));
+//does type conversion
+console.log(Math.max(4, 2, '23', 12, 22));
+console.log(Math.max(4, 2, '23px', 12, 22));
+//Min
+console.log(Math.min(4, 2, 122, 12, 22));
+//Calculate the area of a circle
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+//Random
+console.log(Math.trunc(Math.random() * 6) + 1);
+//Random number between range
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() * (max - min) + 1) + min;
+console.log(randomInt(10, 20));
+
+//Rounding integers
+console.log(Math.trunc(23.3));
+//Round
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+//Ceil
+console.log(Math.ceil(23.3));
+console.log(Math.round(23.9));
+//Floor
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
+//all of these methods do type conversion
+console.log(Math.floor('23.9'));
+
+//With negative numbers it works the other way around
+console.log('/// NEGATIVE NUMBERS ///');
+console.log(Math.floor(-23.3));
+console.log(Math.floor(-23.9));
+console.log(Math.ceil(-23.3));
+console.log(Math.ceil(-23.9));
+console.log(Math.round(-23.3));
+console.log(Math.round(-23.9));
+console.log(Math.trunc(-23.3));
+console.log(Math.trunc(-23.9));
+
+//Rounding decimals
+console.log('/// ROUNDING DECIMALS ///');
+console.log((2.7).toFixed(0)); //to fixed returns a string
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2)); //converting to number with +
