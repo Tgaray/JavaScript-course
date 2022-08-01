6b03bbebed4654c79e57589b440f5d585012e3de
Theron-Avans <therongaray@gmail.com>
2022-08-01 13:05:36 +0200

    Section 13 - Advanced DOM and Events
    Lesson 185 - How the DOM really works
    
    DOM is interface between JS code and browser. No code yet just an explanation.
    
    Lesson 186 - Selecting, Creating and Deleting Elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,61 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////////
+///////////// TESTS ///////////////
+///////////////////////////////////
+
+/// SELECTING ELEMENTS ///
+
+//Selecting the document (entire HTML)
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+//Query selectors
+const header = document.querySelector('.header');
+//Gives a nodelist of all the selected nodes
+//If something gets live deleted from nodelist it still exists in the nodelist
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+//Getelement by ID or Class
+document.getElementById('section--1');
+//getElementsByTagName Gives a live HTML collection is different from node list
+//Which means if i delete something from the HTML the collection is also updated
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+//By classname
+console.log(document.getElementsByClassName('btn'));
+
+/// CREATING AND INSERTING ELEMENTS ///
+
+// .insertAdjacentHTML (in bankist app to create movements) quick and easy
+//Creating a div and storing it in the const but not yet in de actual DOM visible
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button> ';
+//Insert in DOM header (as first child)
+//header.prepend(message);
+
+//add as last child (message can only be added once because its a live element in the DOM) so last thing applies -> appends
+header.append(message);
+
+//making a copy with cloneNode so we will have multiple possible messages (and true for all the children)
+//header.append(message.cloneNode(true));
+
+//before header element as sibling
+//header.before(message);
+//after header as sibling
+//header.after(message);
+
+/// DELETING ELEMENTS ///
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
