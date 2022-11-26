e814dc6648e37b3ac66b3df6a68c52d98db3a900
Tgaray <therongaray@gmail.com>
2022-11-26 16:44:19 +0100

    Section 15 - Mapty
    -Geolocation
    -CDN's
    -Basic map code for tiles + marker

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -20,6 +20,17 @@ if (navigator.geolocation) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 16);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords).addTo(map).bindPopup('My current location.').openPopup();
     },
     //When not giving permission or an error
     function () {
