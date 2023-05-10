f6fb5afa2e1d1a009953bd7dc3cd2e996276c4b6
Theron Garay <therongaray@gmail.com>
2023-05-10 15:34:07 +0200

    Section 16 - Asynchronous JavaScript
    Lesson 265 - Running promises in parallel (Using a combiner function promises.all)

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -569,7 +569,7 @@ console.log('1: I will get the location');
 // console.log('FIRST');
 
 //Above we are combining the old and new ways of writing async functions/code
-//With iffy (immediate revoked function calls) we solve this in this case with an async iffy
+//With iffy (immediate invoked function calls) we solve this in this case with an async iffy
 (async function () {
   try {
     const city = await whereAmI();
@@ -589,3 +589,27 @@ console.log('1: I will get the location');
 // } catch (err) {
 //   alert(err.message);
 // }
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+    //As a promise they run at the same time instead of one after the other like above
+    //This will return one promise that runs the promises in parallel
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+
+    // console.log(data1.capital, data2.capital, data3.capital);
+    //console.log(data);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
