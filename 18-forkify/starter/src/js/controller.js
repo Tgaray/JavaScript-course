508448ec16f618f5416c304758672fa602638ab6
Tgaray <therongaray@gmail.com>
2023-08-04 21:58:32 +0200

    18-Forkify
    Lesson 288 - Loading a recipe from API

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -11,3 +11,30 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd0bc'
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message}, (${res.status})`);
+    //destructuring the object into a new variable (no recipe needed because we destructure into the same namespace, data.data.recipe.
+    let { recipe } = data.data;
+    //renaming/reformatting the object (to remove underscores)
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
